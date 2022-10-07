ad376f316cabb4fdee892484f58d0e4447546b2c
Faith Cox <fcox662@gmail.com>
2022-10-07 16:26:36 -0400

    chapter 5 and 6

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,13 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals
-from views.animal_requests import get_single_animal
+from views.animal_requests import create_animal, delete_animal, get_single_animal, update_animal
 from views import get_all_locations
-from views.location_requests import get_single_location
-
+from views.location_requests import create_location, delete_location, get_single_location
+from views import get_all_employees
+from views.employee_requests import create_employee, delete_employee, get_single_employee
+from views import get_all_customers
+from views.customer_requests import create_customer, delete_customer, get_single_customer
 
 
 # Here's a class. It inherits from another class.
@@ -34,7 +37,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         return (resource, id)  # This is a tuple
     # This is a Docstring it should be at the beginning of all classes and functions
     # It gives a description of the class or function
-    #"""Controls the functionality of any GET, PUT, POST, DELETE requests to the server
+    # """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
    # """
 
     # Here's a class function
@@ -66,23 +69,90 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         self.wfile.write(json.dumps(response).encode())
 
+        if resource == "employees":
+            if id is not None:
+                response = get_single_employee(id)
+
+            else:
+                response = get_all_employees()
+
+        self.wfile.write(json.dumps(response).encode())
+
+        if resource == "customers":
+            if id is not None:
+                response = get_single_customer(id)
+
+            else:
+                response = get_all_customers()
+
+        self.wfile.write(json.dumps(response).encode())
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server"""
-
-        # Set response code to 'Created'- 201
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = {"payload": post_body}
-        self.wfile.write(json.dumps(response).encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_location = None
+        new_employee = None
+        new_customer = None
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_animal).encode())
+
+        #(resource, id) = self.parse_url(self.path)
+
+        # Add a new location to the list. Don't worry about
+        # the orange squiggle, you'll define the create_location
+        # function next.
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+        # Encode the new location and send in response
+        self.wfile.write(json.dumps(new_location).encode())
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+        # Encode the new employee and send in response
+        self.wfile.write(json.dumps(new_employee).encode())
+
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+        # Encode the new customer and send in response
+        self.wfile.write(json.dumps(new_customer).encode())
 
     # A method that handles any PUT request.
     def do_PUT(self):
-        """Handles PUT requests to the server"""
-        self.do_PUT()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+    # Parse the URL
+    (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+    if resource == "animals":
+        update_animal(id, post_body)
+
+    # Encode the new animal and send in response
+    self.wfile.write("".encode())
 
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
@@ -109,9 +179,42 @@ class HandleRequests(BaseHTTPRequestHandler):
                          'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        if resource == "employees":
+            delete_employee(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        if resource == "locations":
+            delete_location(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
+
+
 def main():
     """Starts the server on port 8088 using the HandleRequests class
     """
